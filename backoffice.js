8811e1a4721a3601e06765431d6c655675376c01
AlessandroTedde <148778362+AlessandroTedde@users.noreply.github.com>
2023-12-09 19:07:13 +0100

    something works

--- backoffice.js
+++ backoffice.js
@@ -1,5 +1,15 @@
-const URL = "https://striveschool-api.herokuapp.com/api/product/";
-
+const endpoint = "https://striveschool-api.herokuapp.com/api/product/";
+const token =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc0ODExZDJjNmEwZDAwMTg0OTVmN2IiLCJpYXQiOjE3MDIxMzQwNDUsImV4cCI6MTcwMzM0MzY0NX0.k0CGv8YyTP3bF6_NasTnxo-A79eoV6aagbZ7HeJ690w";
+const resourceId = new URLSearchParams(window.location.search).get("resourceId");
+console.log(resourceId);
+if (resourceId) {
+  method = "PUT";
+  URL = endpoint + resourceId;
+} else {
+  method = "POST";
+  URL = endpoint;
+}
 const addProduct = {
   name: document.getElementById("name").value,
   description: document.getElementById("description").value,
@@ -8,15 +18,77 @@ const addProduct = {
   price: document.getElementById("price").value,
 };
 
-fetch(URL, {
-  method,
-  body: JSON.stringify(addProduct),
-  headers: {
-    "Content-Type": "application/json",
-  },
-})
-  .then((resp) => resp.json())
-  .then((newObject) => {
-    showAlert("Risorsa con id: " + newObject._id + " creata con successo!");
+window.addEventListener("DOMContentLoaded", () => {
+  const confirmButton = document.getElementById("confirm");
+  const trashCan = document.getElementById("delete");
+  const formDesc = document.getElementById("formTitle");
+
+  if (resourceId) {
+    formDesc.innerText = "Modify Product Form";
+    confirmButton.innerText = "Modify Product";
+    confirmButton.classList.remove("btn-primary");
+    confirmButton.classList.add("btn-warning");
+
+    fetch(URL, {
+      headers: {
+        Authorization: token,
+      },
+    })
+      .then((resp) => resp.json())
+      .then(({ name, description, brand, imageUrl, price }) => {
+        document.getElementById("name").value = name;
+        document.getElementById("description").value = description;
+        document.getElementById("brand").value = brand;
+        document.getElementById("imageUrl").value = imageUrl;
+        document.getElementById("price").value = price;
+      });
+  } else {
+    trashCan.classList.add("d-none");
+  }
+});
+const inTheTrash = () => {
+  const confirmedErase = confirm("Do you really want to delete this product?");
+  if (confirmedErase) {
+    fetch(URL, {
+      method: "DELETE",
+      headers: {
+        Authorization: token,
+      },
+    })
+      .then((resp) => {
+        if (resp.ok) {
+          return resp.json();
+        }
+      })
+      .then((deletedProduct) => {
+        window.alert("You have deleted " + deletedProduct.name + " which had id n: " + deletedProduct._id);
+      });
+  }
+};
+const confirm = document.getElementById("confirm");
+confirm.addEventListener("submit", () => {
+  fetch(URL, {
+    method,
+    body: JSON.stringify(addProduct),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token,
+    },
   })
-  .catch((error) => console.log(error));
+    .then((response) => {
+      console.log(response);
+
+      if (response.status === 404) throw new Error("Error, resource not found");
+      if (response.status >= 400 && response.status < 500) throw new Error("Client Error");
+      if (response.status >= 500 && response.status < 600) throw new Error("Server Error");
+      if (!response.ok) throw new Error("Error filling data");
+
+      return response.json();
+    })
+    .then((newProduct) => {
+      resourceId
+        ? window.alert("You have successfully modified your product with id n: " + newProduct._id + " !")
+        : window.alert("You have successfully added your product! The id n. is: " + newProduct._id);
+    })
+    .catch((error) => console.log(error));
+});
--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-const URL = "https://striveschool-api.herokuapp.com/api/product/";
-fetch(URL)
-  .then((resp) => {
-    if (resp.ok) {
-      return resp.json();
-    }
-  })
-  .then((respObj) => {
-    const row = document.getElementById("wrapper");
-    console.log(respObj);
+const endpoint = "https://striveschool-api.herokuapp.com/api/product/";
+const token =
+  "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc0ODExZDJjNmEwZDAwMTg0OTVmN2IiLCJpYXQiOjE3MDIxMzQwNDUsImV4cCI6MTcwMzM0MzY0NX0.k0CGv8YyTP3bF6_NasTnxo-A79eoV6aagbZ7HeJ690w";
 
-    respObj.forEach((obj) => {
+fetch(endpoint, {
+  headers: {
+    Authorization: token,
+  },
+})
+  .then((resp) => resp.json())
+  .then((products) => {
+    const row = document.getElementById("wrapper");
+    console.log(products);
+
+    products.forEach((product) => {
+      const id = product._id;
       const col = document.createElement("div");
       col.classList.add("col");
       const card = document.createElement("div");
       card.classList.add("card");
       const h3 = document.createElement("h3");
-      h3.innerText = obj.name;
+      h3.innerText = product.name;
       const img = document.createElement("img");
-      img.src = obj.img;
+      img.src = product.imageUrl;
       img.classList.add("img-fluid");
       const brand = document.createElement("p");
-      img.classList.add("fs-5");
-      p.innerText = "brand: " + obj.brand;
+      brand.innerText = "brand: " + product.brand;
       const desc = document.createElement("p");
-      img.classList.add("fs-5");
-      p.innerText = obj.description;
+      desc.classList.add("fs-5");
+      desc.innerText = product.description;
       const price = document.createElement("p");
-      img.classList.add("fs-5");
-      p.innerText = "price: " + obj.price;
+      price.classList.add("fs-5");
+      price.innerText = "price: " + product.price + "€";
       const detailsLink = document.createElement("a");
+      detailsLink.setAttribute("href", "./backoffice.html" + "?resourceId=" + id);
       detailsLink.innerText = "Discover More...";
-      detailsLink.href = "./backoffice.html";
+      row.appendChild(col);
+      col.appendChild(card);
+      card.appendChild(img);
+      card.appendChild(h3);
+      card.appendChild(desc);
+      card.appendChild(price);
+      card.appendChild(detailsLink);
     });
   })
   .catch((error) => console.log(error));
